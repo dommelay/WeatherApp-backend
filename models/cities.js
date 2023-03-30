d0d076c28b8f0669eedcb02a0c7a328b03c2882c
Dominique Clark <melaydominique@gmail.com>
2023-03-30 17:57:43 -0400

    commit

--- models/cities.js
+++
@@ -1,9 +0,0 @@
-const mongoose = require('mongoose')
-
-const citiesSchema = new mongoose.Schema({
-   code: Object
-})
-
-const Cities = mongoose.model('Cities', citiesSchema)
-
-module.exports = Cities
---
+++ models/city.js
@@ -0,0 +1,21 @@
+const mongoose = require('mongoose')
+
+const citySchema = new mongoose.Schema({
+   name: String,
+   temp: Number,
+   tempMax: Number,
+   tempMin: Number,
+   tempFeels: Number,
+   time: String,
+   humidity: Number,
+   windSpeed: Number,
+   weatherMain: String,
+   weatherDescription: String,
+   weatherIcon: String,
+   cloud: Number,
+   country: String
+})
+
+const City = mongoose.model('City', citySchema)
+
+module.exports = City
