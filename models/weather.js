23f2d58f7cd9837455d6ad1059a728ce7fd0bb92
Dominique Clark <melaydominique@gmail.com>
2023-03-30 13:14:09 -0400

    working

--- models/weather.js
+++
@@ -1,9 +0,0 @@
-const mongoose = require('mongoose')
-
-const weatherSchema = new mongoose.Schema({
-    city: String
-})
-
-const Weather = mongoose.model('Weather', weatherSchema)
-
-module.exports = Weather
